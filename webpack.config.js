b17a07bb643f979b65a273f152832d728e650f1b
geekestli <3162853966@qq.com>
2022-02-21 23:09:39 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	target: "node",
+	mode: "production",
+
+  entry: './src/app.js',
+  output: {
+    filename: 'app.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
